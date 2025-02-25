6ca08e986936a6a0a80aeefce2b469cbb3dafaea
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-02-25 15:35:46 +0330

    update files

--- groups.py
+++ groups.py
@@ -27,8 +27,21 @@ def create():
         users = {"users":[current_user.username], "leader":current_user.username}
         new_group = Group(name=_name, gender=current_user.data.get("gender", 0), tag=current_user.data.get("tag", 0), score=0, diamonds=0 , users=users, icon=data.get("icon", ""))
         new_group.save()
-       
+        remove_event = []
+        messages = current_user.data.get("message", [])
+        for message in messages:
+            if message.get("type", "") == "join":
+                remove_event.append(message.get("data", {"user":""}).get("user"))
+                messages.remove(message)
         users_sended_message = current_user.data.get("users_sended_message", [])
+        request_message = current_user.data.get("users_request", [])
+        for u in request_message:
+            user2 = User.get_user_by_username(u[0])
+            user_message = user2.data.get("message", [])
+            for m in user_message:
+                if m.get("id", "") == u[1]:
+                    user_message.remove(m)
+            user2.update(data={"message":user_message})
         for u in data.get("event_user", []):
             user = User.get_user_by_username(u.get("username", ""))
             if user is not None:
@@ -37,7 +50,14 @@ def create():
                 message.append({"text":f"{messager_neme} شما را به عضویت در گروه {_name} دعوت کرده است. آیا این دعوت را می پذیرید؟ پس از آن خروج از گروه ممکن نیست", "data":{"group_name": _name}, "time":time.time(), "id":str(uuid.uuid4()), "sender":"کاربر"})
                 user.data = user.update(data={"message":message})
                 users_sended_message.append(user.username)
-        current_user.update(data={"group_name":_name, "users_sended_message":users_sended_message})
+        for u in remove_event:
+            user2 = User.get_user_by_username(u)
+            if user2 is not None:
+                sended_message = user2.data.get("users_sended_message", [])
+                if current_user.username in sended_message:
+                    sended_message.remove(current_user.username)
+                user2.update(data= {"users_sended_message":sended_message})
+        current_user.update(data={"group_name":_name, "users_sended_message":users_sended_message, "message":messages, "users_request":[]})
         db.session.commit()
         return jsonify({"message":"successe"})
     return jsonify({"message":"GROUP name exist"})
