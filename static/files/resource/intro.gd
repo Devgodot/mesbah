b21fc988ae75d34b37b0b2df0f3cf46d1236a421
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-05-28 21:21:39 +0330

    update files

--- static/files/resource/intro.gd
+++ static/files/resource/intro.gd
@@ -5,10 +5,7 @@ var plugin_name = "GodotGetImage"
 var version = "1.3"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Engine.has_singleton("GodotGetFile"):
-		plugin = Engine.get_singleton("GodotGetFile")
-		plugin.startMyForegroundServiceIfNotRunning()
-	await get_tree().create_timer(1).timeout
+	
 	var user = Updatedate.load_user()
 	if user:
 		var data = await Updatedate.load_from_server()
@@ -31,7 +28,6 @@ func _ready() -> void:
 				
 		else:
 			get_tree().reload_current_scene()
-			
 	else:
 		Transation.change(self, "register.tscn")
 
