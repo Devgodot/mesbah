ce759f96751cb494d4bce59fcd6298f90de1ce00
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-05-29 22:12:18 +0330

    update files

--- static/files/resource/object.gd
+++
@@ -1,7 +0,0 @@
-extends Object
-class_name DictObj
-
-var image_height = 400
-var image_width = 400
-var keep_aspect = false
-var image_format = "jpg"
--- static/files/resource/tab_settings.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name TabSettings
-@export var font:Font
-@export var font_size:int
-@export var selected_color:Color
-@export var unselected_color:Color
-@export var outline_size:int
-@export var ouline_selected_color:Color
-@export var outline_unselected_color:Color
