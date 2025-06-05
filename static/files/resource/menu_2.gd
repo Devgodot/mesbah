bc3396bb8bc219699f13bd15d3160e37953913b2
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-06-06 01:41:16 +0330

    update files

--- static/files/resource/menu_2.gd
+++ static/files/resource/menu_2.gd
@@ -25,6 +25,7 @@ func _ready() -> void:
 				Updatedate.p_scene = scene_file_path.get_file().get_basename()
 				Transation.change(self, "positions.tscn")
 				)
+	await get_tree().create_timer(0.1).timeout
 	show()
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
