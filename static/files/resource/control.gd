29d066e1135f74b83a69d0820fc8d85fa6949608
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-08-17 21:26:49 +0330

    update files

--- static/files/resource/control.gd
+++ static/files/resource/control.gd
@@ -378,6 +378,7 @@ func _on_button_pressed() -> void:
 		add_message(m)
 		Updatedate.send_message(text_edit.text, id, box_ref.get_meta("id", "") if box_ref else "")
 		text_edit.text = ""
+		await get_tree().create_timer(0.1).timeout
 		$VBoxContainer/ScrollContainer.scroll($VBoxContainer/ScrollContainer.max_scroll)
 		_on_null_ref_pressed()
 func _on_back_button_pressed() -> void:
