d7d13b38566d256b2c2bb420f2f02e89b3369fdd
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-09-26 00:06:29 +0330

    update files

--- static/files/resource/intro.gd
+++ static/files/resource/intro.gd
@@ -5,6 +5,7 @@ var plugin_name = "GodotGetImage"
 var version = "1.3"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	var user = Updatedate.load_user()
 	await update_resource()
 	if user:
@@ -39,7 +40,7 @@ func update_resource():
 	if subdomin != "127.0.0.1:5000" and internet:
 		var http = HTTPRequest.new()
 		add_child(http)
-		var last_update = "1758807922136"
+		var last_update = "1758809698140"
 		if float(Updatedate.load_game("last_update", "0")) > float(last_update):
 			last_update = Updatedate.load_game("last_update", "0")
 		else:
@@ -55,6 +56,7 @@ func update_resource():
 			d = await http.request_completed
 		http.queue_free()
 		var data = Updatedate.get_json(d[3])
+		print(data)
 		if data and not data.has("error"):
 			if not DirAccess.dir_exists_absolute("user://resource"):
 				DirAccess.make_dir_absolute("user://resource")
@@ -71,6 +73,7 @@ func update_resource():
 				http2.request(protocol+subdomin+"/static/files/pack/"+file, ["Content-Type: application/json"])
 				await http2.request_completed
 				http2.queue_free()
+				print(FileAccess.get_file_as_bytes("user://update/"+file).size())
 				ProjectSettings.load_resource_pack("user://update/"+file)
 				index += 1
 				http2.queue_free()
@@ -88,5 +91,5 @@ func update_resource():
 				index += 1
 				http2.queue_free()
 			Updatedate.save("last_update", str(data.time), false)
-			await get_tree().create_timer(0.5).timeout
+			await get_tree().create_timer(1.5).timeout
 			Updatedate.end_download.emit()
